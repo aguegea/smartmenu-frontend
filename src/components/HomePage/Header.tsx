6eb81a65c0cf1296fe685a7d1249128527cc708d
aguegea <aguegea_04@hotmail.com>
2024-11-25 14:57:20 -0300

    Changed styles in header

--- src/components/HomePage/Header.tsx
+++ src/components/HomePage/Header.tsx
@@ -4,12 +4,13 @@ export const Header: React.FC = () => {
     return (
         <header className="bg-neutral text-center py-10">
             <h1 className="text-5xl font-bold text-primary mb-6">SmartMenu</h1>
-            <p className="text-lg text-accent max-w-2xl mx-auto">
+            <p className="text-lg text-accent max-w-2xl mx-auto px-4 sm:px-6">
                 Tired of spending thousands of dollars on software for the
                 management of your restaurant?
                 <br />
-                SmartMenu brings you the solution. Upload your plates, create
-                and delete orders from $11.99 per month.
+                SmartMenu brings you the solution.
+                <br />
+                Upload your plates, create and delete orders from $11.99 per month.
             </p>
         </header>
     );

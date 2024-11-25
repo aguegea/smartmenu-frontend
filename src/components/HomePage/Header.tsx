0f283d300f7ac61dda1fb0db2d0ef53b49c1d483
aguegea <aguegea_04@hotmail.com>
2024-11-25 15:02:22 -0300

    Changed styles for title

--- src/components/HomePage/Header.tsx
+++ src/components/HomePage/Header.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export const Header: React.FC = () => {
     return (
         <header className="bg-neutral text-center py-10">
-            <h1 className="text-5xl font-bold text-primary mb-6">SmartMenu</h1>
+            <h1 className="text-5xl font-bold text-white bg-gradient-to-br from-primary to-secondary bg-clip-text text-transparent mb-6 drop-shadow-lg">
+                SmartMenu
+            </h1>
             <p className="text-lg text-accent max-w-2xl mx-auto px-4 sm:px-6">
                 Tired of spending thousands of dollars on software for the
                 management of your restaurant?
